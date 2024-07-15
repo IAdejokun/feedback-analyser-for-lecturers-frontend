74faeb771391d57d82206314293b0c94a379fbfe
Adejokun Ibukunoluwa <Adejokunibk@gmail.com>
2024-07-15 13:41:39 +0100

    changes made to the route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { LoginComponent } from './pages/login/login.component';
 import { GetStartedComponent } from './pages/get-started/get-started.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { GetHelpComponent } from './pages/get-help/get-help.component';
+import { LecturerDashboardComponent } from './pages/lecturer/lecturer-dashboard/lecturer-dashboard.component';
+import { StudentDashboardComponent } from './pages/student/student-dashboard/student-dashboard.component';
 
 export const routes: Routes = [
     {
@@ -17,6 +19,14 @@ export const routes: Routes = [
     {
         path:'login',
         component:LoginComponent
+    },
+    {
+        path:'lecturer/dashboard',
+        component:LecturerDashboardComponent
+    },
+    {
+        path:'student/dashboard',
+        component:StudentDashboardComponent
     },
     {
         path:'get-help',
